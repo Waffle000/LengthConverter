a18aaa2cc338a640759f81810d2ddd7bfc47d5bf
Waffle <enrico.maricar@gmail.com>
2024-06-25 13:30:40 +0700

    Initial Commit

---
+++ LengthConvertion/LengthConvertionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConvertionApp.swift
+//  LengthConvertion
+//
+//  Created by Enrico Maricar on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LengthConvertionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
